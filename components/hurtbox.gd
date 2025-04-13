359b2b9b206dd034ddc29acb4cffaff41ba64083
pablocoop <pabloosorionavarro@gmail..com>
2025-04-12 22:51:57 -0400

    FEAT enemy scene with neutral side movement

--- components/hurtbox.gd
+++ components/hurtbox.gd
@@ -5,5 +5,9 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	
 func _on_area_entered(area: Area2D) -> void:
-	Debug.log("hurtbox")
-	Debug.log(area)
+	var hitbox = area as Hitbox
+	if hitbox:
+		if owner.has_method("take_damage"):
+			owner.take_damage(hitbox.damage)
+			hitbox.damage_dealt.emit()
+	
