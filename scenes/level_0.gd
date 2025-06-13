ace4e437b7ad5d75adbcb7fd5fd5a9c84376823d
Aldo431 <aldoluna75@gmail.com>
2025-06-12 23:17:29 -0400

    feat: se agrego la implementacion del portal de cambio de nivel

--- scenes/level_0.gd
+++ scenes/level_0.gd
@@ -5,9 +5,20 @@ extends Node2D
 var current_time_scale := 1.0
 var target_time_scale := 1.0
 @export var smooth_transition_speed := 5.0
+@onready var portal_1: Area2D = $Portal_1
 
+var enemy_count := 0
 
 func _ready():
+	# Busca enemigos al iniciar
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		enemy_count += 1
+		enemy.enemy_died.connect(_on_enemy_died)
+
+	# Portal oculto y no activo al inicio
+	portal_1.hide()
+	portal_1.ready_portal = 0
+	
 	ball.ball_stopped.connect(_on_ball_stopped)
 	ball.ball_relaunched.connect(_on_ball_relaunched)
 	ball.ball_speed_changed.connect(_on_ball_speed_changed)  # Nueva conexión
@@ -36,3 +47,10 @@ func _on_ball_speed_changed(factor: float) -> void:
 	# en caso de que quieras controlar ellos directamente:
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		enemy._on_slowmo_factor(factor)
+		
+func _on_enemy_died():
+	enemy_count -= 1
+	print("☠️ Enemigo eliminado. Quedan:", enemy_count)
+	if enemy_count <= 0:
+		portal_1.show()
+		portal_1.ready_portal = 1
